c3958232f964ac477025a9408bd70f2ff65364d8
M4rqu1705 <marcospesante11@gmail.com>
2018-07-10 19:45:22 +0000

    Second Commit

--- forward.py
+++ forward.py
@@ -1,3 +1,17 @@
-from talker.py import publish_message
+#!/usr/bin/env python
 
-publish_message('forward.py', '/vesc/ackermann_cmd_mux/input/navigation/ackerman_msgs/AckermannDriveStamped', '{header: auto, drive: {steering_angle:0.2, speed:1.2}}')
+import rospy
+from ackermann_msgs.msg import AckermannDriveStamped
+
+def publish_message(origin, destination):
+    rospy.init_node(origin,  anonymous=True)
+    pub = rospy.Publisher(destination, AckermannDriveStamped, queue_size=10)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+	msg = AckermannDriveStamped()
+	msg.drive.speed = 1
+	msg.drive.steering_angle = 0
+	pub.publish(msg)
+	rate.sleep()
+
+publish_message('forward_node', '/vesc/high_level/ackermann_cmd_mux/input/nav_0')
