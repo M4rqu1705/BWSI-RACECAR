4fd094c4ee3359ee52e4e4bc1d1a579e0c495055
M4rqu1705 <m4rc05.dev@gmail.com>
2018-07-18 19:15:56 +0000

    Updated safety controller

---
+++ week2/projects/week_1_pf/PFController.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import rospy
+import math
+import numpy as np
+from sensor_msgs.msg import LaserScan
+class pfController:
+
+    steeringScale = None
+    speedScale = None
+    backChargeSize = None
+    totalReadings = None
+
+    def __init__(self, inputSteeringScale, inputSpeedScale,  inputBackChargeSize):
+        self.steeringScale = inputSteeringScale
+        self.speedScale = inputSpeedScale
+        self.backChargeSize = inputBackChargeSize
+
+    def compute(self, scan):
+        self.totalReadings = len(scan)
+        myScan = self.partialScan(self.totalReadings/4, (3*self.totalReadings)/4, scan)
+        partialReadings = len(myScan)
+        angles = np.arange(self.totalReadings/4, (3*self.totalReadings)/4, dtype=float)
+        angles = np.multiply(angles, (2*math.pi)/self.totalReadings)
+
+        xArray = np.empty(partialReadings, dtype = float)
+        yArray = np.empty(partialReadings, dtype = float)
+
+        xArray = np.true_divide(np.cos(angles), np.power(myScan, .2))
+        yArray = np.true_divide(np.sin(angles), np.power(myScan, .2))
+
+
+        finalX = (np.sum(xArray)+ self.backChargeSize) * self.speedScale
+        finalY = np.sum(yArray)*self.steeringScale
+
+        return(finalY, finalX)
+
+    def partialScan(self, start, end, scan):
+        return (scan[start:end])
--- week2/tools/safety_controller.py
+++ week2/tools/safety_controller.py
@@ -8,28 +8,52 @@ from ackermann_msgs.msg import AckermannDriveStamped
 
 class safety_controller:
 
-    laser_ranges = np.array([x for x in range(0,897)])
-    counter = 0
+    laser_readings = np.array([x for x in range(0,897)])    # Prepare initial thrash values for laser readings
+    readings_degrees = np.linspace(0, 360, num=897)         # Prepare mappings from laser readings to degrees
+    counter = 0                                             # Preset the counter to 0
+    important_indices = [0, 0, (0, 0), 0, 0]                # Preset the importnt_indices list to 0
 
-    def __init__(self):              
+    def __init__(self, amount_readings, amount_degrees):              
+        global laser_readings
+        global important_indices
+
+        # Create subscriber from which to read LIDAR readings
         self.laser_sub = rospy.Subscriber("/scan_processed", LaserScan, self.laser_callback, queue_size=1)  
+
+        # Create subscriber from which to read requests to move from other programs
         self.drive_sub = rospy.Subscriber("/vesc/high_level/ackermann_cmd_mux/output", AckermannDriveStamped, self.drive_callback, queue_size = 1)                
+
+        # Create publisher with which to update RACECAR speed and steering angle
         self.drive_pub = rospy.Publisher("/vesc/low_level/ackermann_cmd_mux/input/safety", AckermannDriveStamped, queue_size=10)    
-        self.stop = False 
-        self.slow_down = False
-        self.distance_thresh_max = 1
-    	self.distance_threshold_min = 0.75
+
+        
+        self.state = "none"                     # String with which to indicate what to do in response of laser readings
+        self.distance_thresholds = [1, 0.5]     # Indicate max and min threshold with which to respond
+        self.amount_readings = amount_readings  # Update the amount of readings the LIDAR does per cycle based on constructor parameters
+        self.amount_degrees = amount_degrees    # Update the range of degrees the LIDAR reads from per cycle based on constructor parameters
+
+        laser_readings = np.array([x for x in range(0, self.amount_readings)])            # Prepare initial thrash values for laser readings
+
+        # Make list containing the following indices: left, diagonal left, front range,  diagonal right, right
+        important_indices = [int((laser_readings.size/2.0)-(90.0/self.amount_degrees*laser_readings.size)),
+                int((laser_readings.size/2.0)-(45.0/self.amount_degrees*laser_readings.size)),
+                [int(laser_readings.size/2)+15, int(laser_readings.size/2)-15 ],
+                int((laser_readings.size/2.0)-(-45.0/self.amount_degrees*laser_readings.size)),
+                int((laser_readings.size/2.0)-(-90.0/self.amount_degrees*amount_readings))]
+        
 
     def laser_callback(self, scan_data):  
-        global laser_ranges
+        global laser_readings
         global counter
+        global important_indices
         
         # Update the global laser_range's value with the new scans
-        laser_ranges = np.array(scan_data.ranges)
-        laser_ranges[laser_ranges==200] = 0.4
+        laser_readings = np.array(scan_data.ranges)
         
-        # Use 20 equally distanced readings from the front part to decide if to stop
-	mid_data_range = np.array(laser_ranges[int(laser_ranges.size/2)-15 : int(laser_ranges.size/2)+15])
+        laser_readings[laser_readings==200] = 0.3       # Values of 200, or capped values, are interpreted as 0.3
+        
+        # Use x equally distanced readings from the front part to decide if to stop
+        mid_data_range = np.array(laser_readings[important_indices[2][1]:important_indices[2][0]])
 
         # --------- Median ------
         
@@ -38,43 +62,41 @@ class safety_controller:
 
         # --------- Median ------
 
-        # Send the stop signal only if the measured obstacle is within the distance threshold or is equal to 200
-        if mid_data_point < self.distance_thresh_max and mid_data_point > self.distance_threshold_min:
-            self.slow_down = True
-            self.stop = False
-        elif mid_data_point < self.distance_threshold_min:  
-            self.stop = True  
-            self.slow_down = False
+
+        # If front distance is within the max limit and min limit
+        if self.distance_thresholds[0] > mid_data_point and  mid_data_point > self.distance_thresholds[1]:
+            self.state = "slow down"
+        # If the side distances are within the max limit
+        elif laser_readings[important_indices[0]] < self.distance_thresholds[0] or laser_readings[important_indices[4]] < self.distance_thresholds[0]:
+            self.state = "slow down"
+        # if the front distance is within the min limit
+        elif mid_data_point <= self.distance_thresholds[1]:  
+            self.state = "reverse"
+        # If everything is alright
         else: 
-            self.stop = False      
-            self.slow_down = False
+            self.state = "none"
+
 	rospy.loginfo(mid_data_range)
 
     def drive_callback(self, drive_msg): 
 
         self.counter+=1
 
-        # If self.stop is true, make the robot follow an algorithm
-        if self.stop:
-            if laser_ranges[int(len(laser_ranges) * 3.0/8.0)] < laser_ranges[int(len(laser_ranges) * 5.0/8.0)]:
-                drive_msg.drive.steering_angle -= 0.05*self.counter
-                #  drive_msg.drive.steering_angle = -1
-            else:
+        if "reverse" in self.state:
+            if laser_readings[important_indices[1]] <= laser_readings[important_indices[3]] and laser_readings[important_indices[0]] <= laser_readings[important_indices[4]]:
                 drive_msg.drive.steering_angle += 0.05*self.counter
-                #  drive_msg.drive.steering_angle = 1
+            else:
+                drive_msg.drive.steering_angle -= 0.05*self.counter
 
             drive_msg.drive.speed -=0.5*self.counter
-            #  drive_msg.drive.speed = -1
 
-        elif self.slow_down:
-            if laser_ranges[int(len(laser_ranges) * 3.0/8.0)] < laser_ranges[int(len(laser_ranges) * 5.0/8.0)]:
-                drive_msg.drive.steering_angle += 0.1*self.counter
-                drive_msg.drive.speed = 0.25*laser_ranges[int(len(laser_ranges) / 2.0)]
-            else:
+        elif "slow down" in self.state:
+            if laser_readings[important_indices[1]] <= laser_readings[important_indices[3]]:
                 drive_msg.drive.steering_angle -= 0.1*self.counter
-                drive_msg.drive.speed = 0.25*laser_ranges[int(len(laser_ranges) / 2.0)]
-
-            #  drive_msg.drive.speed = -1
+                drive_msg.drive.speed = laser_readings[important_indices[1]]/1.25
+            else:
+                drive_msg.drive.steering_angle += 0.1*self.counter
+                drive_msg.drive.speed = laser_readings[important_indices[3]]/1.25
 
         else:
            self.counter = 0 
@@ -82,5 +104,5 @@ class safety_controller:
 
 if __name__ == "__main__":
     rospy.init_node("safety_controller") 
-    node = safety_controller()                  
+    node = safety_controller(897, 360)                  
     rospy.spin()  
